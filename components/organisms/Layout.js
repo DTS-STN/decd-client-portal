2c466b8cc5ffe88a62e8ad2b8c00a92cfadebef6
arsani <arsanig@users.noreply.github.com>
2021-11-10 09:40:16 -0500

    Early components and structure

---
+++ components/organisms/Layout.js
@@ -0,0 +1,12 @@
+import Header from '../molecules/Header'
+import Footer from '../molecules/Footer'
+
+export default function Layout() {
+  return (
+    <div>
+      <Header />
+      This is for layout.
+      <Footer />
+    </div>
+  )
+}
---
+++ pages/dashboard.js
@@ -0,0 +1,5 @@
+import Layout from '../components/organisms/Layout'
+
+export default function Dashboard() {
+  return <Layout />
+}
