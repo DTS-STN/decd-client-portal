260550a1cef954b1f507b8aa75315bd28dcf62af
nhamade <nhamade@gmail.com>
2021-11-29 13:01:40 -0500

    add more communications data into payload

--- pages/api/communications.js
+++ pages/api/communications.js
@@ -15,6 +15,28 @@ export default function handler(req, res) {
             attachmentInd: 'false',
           },
         },
+        {
+          UACommunication: {
+            communication_id: 'COM0001113',
+            subjectText: 'Over Payment Letter',
+            communicationDate: '01012020',
+            coverEndDate: '01012025',
+            userName: 'cnd',
+            incomingInd: 'false',
+            attachmentInd: 'false',
+          },
+        },
+        {
+          UACommunication: {
+            communication_id: 'COM0001113',
+            subjectText: 'Underpayment Payment Letter',
+            communicationDate: '01012020',
+            coverEndDate: '01012025',
+            userName: 'cnd',
+            incomingInd: 'false',
+            attachmentInd: 'false',
+          },
+        },
       ],
     },
   })
