d07eb054a72427dbdad72538262dedfb31c6b9e1
nhamade <nhamade@gmail.com>
2021-12-06 13:28:26 -0500

    modfied message notification font; add section status headings

--- components/molecules/MessageNotification.js
+++ components/molecules/MessageNotification.js
@@ -3,14 +3,17 @@ import React, { useState } from 'react'
 const MessageNotification = () => {
   return (
     <div className="layout-container">
-      <div>
-        <p>
-          <img src="/notification.svg" className="inline" /> 1 messages.{' '}
+      <div className="py-4">
+        <p className="text-2xl">
+          <img src="/notification.svg" className="inline" />
+          <span className="notification-blue-text text-2xl"> 1</span> New
+          Message{' '}
           <a href="#">
             <u>Go to Notifications and Message Centre</u>
           </a>
         </p>
       </div>
+      <hr className="bg-bright-blue-royal h-tiny" />
     </div>
   )
 }
