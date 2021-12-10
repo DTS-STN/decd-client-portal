b6d56b353c0c6a471026974be084bd834e49ca8a
arsani <arsanig@users.noreply.github.com>
2021-12-10 13:59:30 -0500

    Whoops, typo

--- components/atoms/LanguageToggle.js
+++ components/atoms/LanguageToggle.js
@@ -7,14 +7,20 @@ export default function LanguageToggle() {
   return (
     <div>
       <div className="hidden md:inline">
-        <Link href={route.asPath} locale={router.locale === 'en' ? 'fr' : 'en'}>
+        <Link
+          href={router.asPath}
+          locale={router.locale === 'en' ? 'fr' : 'en'}
+        >
           <a className="font-medium">
             {router.locale === 'en' ? 'Français' : 'English'}
           </a>
         </Link>
       </div>
       <div className="ml-7 md:hidden">
-        <Link href={route.asPath} locale={router.locale === 'en' ? 'fr' : 'en'}>
+        <Link
+          href={router.asPath}
+          locale={router.locale === 'en' ? 'fr' : 'en'}
+        >
           <a className="font-bold">{router.locale === 'en' ? 'FR' : 'EN'}</a>
         </Link>
       </div>
