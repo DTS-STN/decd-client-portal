bd118b86eb6fdb3c2fcec21f8d063e977f53d871
Amir Pourpanahi <51453299+amirpourpanahi@users.noreply.github.com>
2021-12-13 15:49:57 -0500

    Adding translation for GIS card

--- components/molecules/GisBenefitCard.jsx
+++ components/molecules/GisBenefitCard.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import useTranslation from 'next-translate/useTranslation'
 
 const GisBenefitCard = (props) => {
+  const { t } = useTranslation('dashboard')
   return (
     <div className="grid grid-cols-1 divide-y divide-grey-500">
       <section></section>
       <section className="mt-4 mx-8" id="paymentDate">
         <div className="sm:flex sm:justify-around sm:grid sm:grid-cols-2 sm:gap-4 sm:mx-auto sm:mt-4">
           <div id="applicationDate">
-            <p>Application Date</p>
+            <p>{t('application_date')}</p>
             <p className="font-bold text-base">
               {props.guaranteedIncomeSupplement.applicationDate}
             </p>
           </div>
           <div id="applicationType">
-            <p>Application Type</p>
+            <p>{t('application_type')}</p>
             <p className="font-bold text-base">
               {props.guaranteedIncomeSupplement.applicationType}
             </p>
           </div>
           <div></div>
           <div id="applicationDescription">
-            <p>Application Description</p>
+            <p>{t('application_description')}</p>
             <p className="font-bold text-base">
               {props.guaranteedIncomeSupplement.applicationDescription}
             </p>
@@ -32,26 +34,26 @@ const GisBenefitCard = (props) => {
       <section className="mt-4 mx-8" id="payeeInfo">
         <div className="sm:flex sm:justify-around sm:grid sm:grid-cols-2 sm:gap-4 sm:mx-auto sm:mt-4">
           <div id="withdrawalRequest" className="mb-4 sm:mb-0">
-            <p>Withdrawal Request</p>
+            <p>{t('withdrawal_request')}</p>
             <p className="font-bold text-base">
               {props.guaranteedIncomeSupplement.withdrawalRequest}
             </p>
           </div>
           <div id="withdrawalStatus" className="mb-4 sm:mb-0">
-            <p>Withdrawal Status</p>
+            <p>{t('withdrawal_status')}</p>
             <p className="font-bold text-base">
               {props.guaranteedIncomeSupplement.withdrawalStatus}
             </p>
           </div>
 
           <div id="withdrawalRequestDescription" className="mb-4 sm:mb-0">
-            <p>Withdrawal Request Description</p>
+            <p>{t('withdrawal_request_description')}</p>
             <p className="font-bold text-base">
               {props.guaranteedIncomeSupplement.withdrawalRequestDescription}
             </p>
           </div>
           <div id="applicationWithdrawn" className="mb-4 sm:mb-0">
-            <p>Application Withdrawn</p>
+            <p>{t('application_withdrawn')}</p>
             <p className="font-bold text-base">
               {props.guaranteedIncomeSupplement.applicationWithdrawn}
             </p>
