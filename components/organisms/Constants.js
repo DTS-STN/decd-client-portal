d9cfb82097392ee4a867fe81a92ed375fd6b9199
nhamade <nhamade@gmail.com>
2021-12-16 18:27:54 -0500

    correct apis urls

--- components/organisms/Constants.js
+++ components/organisms/Constants.js
@@ -1,14 +1,14 @@
 export const CPPBENEFIT =
-  'http://decd-client-portal-main.bdm-dev.dts-stn.com/api/cppbenefits'
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/cppbenefits'
 export const OASBENEFIT =
-  'http://decd-client-portal-main.bdm-dev.dts-stn.com/api/oasbenefits'
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/oasbenefits'
 export const EIBENEFIT =
-  'http://decd-client-portal-main.bdm-dev.dts-stn.com/api/eibenefits'
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/eibenefits'
 export const USER =
-  'http://decd-client-portal-main.bdm-dev.dts-stn.com/api/user'
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/user'
 export const PAYMENTS =
-  'http://decd-client-portal-main.bdm-dev.dts-stn.com/api/payments'
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/payments'
 export const COMMUNICATION =
-  'http://decd-client-portal-main.bdm-dev.dts-stn.com/api/communications'
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/communications'
 export const SUBMITTEDAPI =
-  'http://decd-client-portal-main.bdm-dev.dts-stn.com/api/submitted-app'
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/submitted-app'
