4ce0652c3adbc989ec4e75a26610e0167ea8528b
Amir Pourpanahi <51453299+amirpourpanahi@users.noreply.github.com>
2021-12-20 13:47:17 -0500

    Constants.js changes was committed by mistake

--- components/organisms/Constants.js
+++ components/organisms/Constants.js
@@ -1,22 +1,14 @@
-export const CPPBENEFIT = 'http://localhost:3000/api/cppbenefits'
-export const OASBENEFIT = 'http://localhost:3000/api/oasbenefits'
-export const EIBENEFIT = 'http://localhost:3000/api/eibenefits'
-export const USER = 'http://localhost:3000/api/user'
-export const PAYMENTS = 'http://localhost:3000/api/payments'
-export const COMMUNICATION = 'http://localhost:3000/api/communications'
-export const SUBMITTEDAPI = 'http://localhost:3000/api/submitted-app'
-
-// export const CPPBENEFIT =
-//   'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/cppbenefits'
-// export const OASBENEFIT =
-//   'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/oasbenefits'
-// export const EIBENEFIT =
-//   'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/eibenefits'
-// export const USER =
-//   'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/user'
-// export const PAYMENTS =
-//   'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/payments'
-// export const COMMUNICATION =
-//   'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/communications'
-// export const SUBMITTEDAPI =
-//   'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/submitted-app'
+export const CPPBENEFIT =
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/cppbenefits'
+export const OASBENEFIT =
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/oasbenefits'
+export const EIBENEFIT =
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/eibenefits'
+export const USER =
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/user'
+export const PAYMENTS =
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/payments'
+export const COMMUNICATION =
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/communications'
+export const SUBMITTEDAPI =
+  'https://decd-client-portal-main.bdm-dev.dts-stn.com/api/submitted-app'
